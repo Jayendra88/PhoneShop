317a1e71284de39b806a3c6dbf3707dc52ef9989
jayendra88 <jayendra.k.a@gmail.com>
2014-02-28 00:07:59 +0530

    3

---
+++ PhoneShop/Models/CustomerM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace PhoneShop.Models
+{
+    public class CustomerM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string NIC { get; set; }
+        public ObservableCollection<TelephoneNumberM> TelephoneNumbers { get; set; }
+
+        public CustomerM() 
+        {
+            TelephoneNumbers = new ObservableCollection<TelephoneNumberM>();
+        }
+    }
+}
